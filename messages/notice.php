0731ac41ccca05d61f20c36a7c94f86e6d64b3e8
Miodrag Tokić <mtokic@gmail.com>
2010-06-10 21:54:34 +0000

    Added some more messages

--- messages/notice.php
+++ messages/notice.php
@@ -1,6 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'no_selected' => 'No :item selected',
-	'no_data'     => 'No data to display',
+	'no_selected'  => 'No :item selected',
+	'no_data'      => 'No data to display',
+	'no_exist'     => ':item does not exist.',
+	'validation'   => 'Data validation failed:',
+	'save_success' => 'Your data has been successfully saved.',
 );
