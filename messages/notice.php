7a6e084bfec3f2a2ca976b1a5f380a28c006c244
Miodrag Tokić <mtokic@gmail.com>
2012-06-29 23:38:13 +0200

    Updated notice messages

--- messages/notice.php
+++ messages/notice.php
@@ -1,9 +1,19 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'no_selected'  => 'No :item selected',
-	'no_data'      => 'No data to display',
-	'no_exist'     => ':item does not exist.',
-	'validation'   => 'Data validation failed:',
-	'save_success' => 'Your data has been successfully saved.',
+	'no_selected'        => 'No :item selected',
+	'no_data'            => 'No data to display',
+	'not_found'          => ':item not found',
+	'validation'         => 'Data validation failed:',
+	'credential_invalid' => 'The credentials you provided are incorrect',
+	'signin_success'     => 'Welcome aboard',
+	'signout_success'    => 'Bye... Hope to see you soon!',
+	'upload_success'     => 'File uploaded successfully',
+	'upload_error'       => 'Error uploading file',
+	'create_success'     => ':item successfuly created',
+	'create_error'       => 'Error creating :item',
+	'update_success'     => ':item successfuly updated',
+	'update_error'       => 'Error updating :item',
+	'delete_success'     => ':item successfuly deleted',
+	'delete_error'       => 'Error deleting :item',
 );
